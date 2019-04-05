1a8f45b5c70f3d665ea752af7d342ec3c5056a7b
wmadwand <wmadwand@gmail.com>
2019-04-05 09:36:19 +0300

    Basic scene elements

---
+++ Assets/Coin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Coin : MonoBehaviour, IPointerClickHandler
+{
+    public static event Action OnDestroy;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		GetComponent<Rigidbody>().AddForce(Vector3.up * 1, ForceMode.Impulse);
+	}
+}
---
+++ Assets/CoinSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawn : MonoBehaviour
+{
+	public GameObject coinPrefab;
+	public Transform spawnPoint;
+
+	bool isNeedCoin;
+
+	private void Awake()
+	{
+		Coin.OnDestroy += Coin_OnDestroy;
+	}
+
+	private void Coin_OnDestroy()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void Update()
+	{
+		if (isNeedCoin)
+		{
+			CreateCoin();
+		}
+	}
+
+	void CreateCoin()
+	{
+		var go = Instantiate(coinPrefab, spawnPoint);
+	}
+}
