487a38e0cc9ddc141320d52418d6e2d4f466985f
wmadwand <wmadwand@gmail.com>
2019-04-05 10:59:49 +0300

    Get coin size increased depending on clicks count

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -6,17 +6,61 @@ using UnityEngine.EventSystems;
 
 public class Coin : MonoBehaviour, IPointerClickHandler
 {
-    public static event Action OnDestroy;
+	public static event Action OnDestroy;
 	public float bounceRate;
+
+	int _clickCount;
+	bool _startSelfDestroy;
+	float _countdownRate = 4;
+
+	private void Update()
+	{
+		if (_startSelfDestroy)
+		{
+			_countdownRate -= Time.fixedDeltaTime;
+
+			if (_countdownRate <= 0)
+			{
+				SelfDestroy();
+			}
+		}
+	}
 
 	public void OnPointerClick(PointerEventData eventData)
 	{
+		if (_startSelfDestroy)
+		{
+			return;
+		}
+
+		_clickCount++;
+
 		GetComponent<Rigidbody>().AddForce(Vector3.up * bounceRate, ForceMode.Impulse);
+
+		Enlarge();
 
 		Debug.Log("click");
 	}
 
 	void Enlarge()
+	{
+		if (_clickCount == 4)
+		{
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+		else if(_clickCount == 8)
+		{
+			transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+			_startSelfDestroy = true;
+		}
+	}
+
+	void SelfDestroy()
+	{
+		Destroy(gameObject);
+	}
+
+	void Blink()
 	{
 
 	}
