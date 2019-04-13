13b1d427c8ba7787aceb7b79a500465bd51b191d
wmadwand <wmadwand@gmail.com>
2019-04-13 16:09:17 +0300

    TubeOut 2D version

---
+++ Assets/Scripts/2DVersion/TubeOut2D.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TubeOut2D : MonoBehaviour
+{
+	public int Id;
+	public GameObject coinPrefab;
+	public Transform spawnPoint;
+
+	bool isRequiredCoin = true;
+
+	public float initialBounceRate;
+
+	private void Awake()
+	{
+		Coin.OnDestroy += Coin_OnDestroy;
+	}
+
+	private void Coin_OnDestroy(int id)
+	{
+		if (Id == id)
+		{
+			isRequiredCoin = true;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.GetComponent<Bubble>())
+		{
+			if (other.GetComponent<Bubble>().IsReleased)
+			{
+				other.GetComponent<Bubble>().SelfDestroy();
+			}
+			else
+			{
+				other.GetComponent<Bubble>().SetReleased();
+			}
+
+
+		}
+	}
+
+	private void Update()
+	{
+		if (isRequiredCoin)
+		{
+			isRequiredCoin = false;
+
+			SomeDelay(CreateCoin);
+			//StartCoroutine(SomeDelayRoutine(CreateCoin));
+		}
+	}
+
+	void CreateCoin()
+	{
+
+		var go = Instantiate(coinPrefab, spawnPoint.position, Quaternion.identity);
+		go.GetComponent<Bubble>().tubeId = Id;
+
+
+		var randomBounceRate = UnityEngine.Random.Range(initialBounceRate, initialBounceRate * 1.7f);
+
+		go.GetComponent<Rigidbody2D>().AddForce(Vector3.up * randomBounceRate, ForceMode2D.Impulse);
+	}
+
+	async void SomeDelay(Action callback)
+	{
+		var delayRate = GameController.Instance.gameSettings.delayCoinThrow ? UnityEngine.Random.Range(.5f, 1.5f) : 0;
+
+		await Task.Delay(TimeSpan.FromSeconds(delayRate));
+		callback();
+	}
+
+	//IEnumerator SomeDelayRoutine(Action callback)
+	//{
+	//	var delayRate = GameController.Instance.gameSettings.delayCoinThrow ? UnityEngine.Random.Range(.5f, 1.5f) : 0;
+
+	//	yield return new WaitForSeconds(delayRate);
+	//	callback();
+	//}
+}
---
+++ Assets/Scripts/2DVersion/TubeSpawner2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TubeSpawner2D : MonoBehaviour
+{
+	public GameObject tubeInPrefab;
+	public GameObject tubeOutPrefab;
+
+	public CoinType[] coinTypeArray;
+
+	public GameObject[] tubeInSpawnPoints;
+	public GameObject[] tubeOutSpawnPoints;
+
+	System.Random _rnd;
+
+	private void Awake()
+	{
+		_rnd = new System.Random();
+	}
+
+	private void Start()
+	{
+		GenerateTubes();
+	}
+
+	void GenerateTubes()
+	{
+
+		//int[] coinTypeArray = { 0, 1, 2 };
+		CoinType[] MyRandomArray = coinTypeArray.OrderBy(x => _rnd.Next()).ToArray();
+
+		for (int i = 0; i < /*2*/ MyRandomArray.Length; i++)
+		{
+			var go = Instantiate(tubeOutPrefab, tubeInSpawnPoints[i].transform.position, Quaternion.identity);
+			go.GetComponent<TubeOut2D>().Id = i;
+
+			var tubeIn = Instantiate(tubeInPrefab, tubeOutSpawnPoints[i].transform.position, Quaternion.identity);
+			tubeIn.GetComponent<TubeIn>().Setup(MyRandomArray[i]);
+
+		}
+	}
+}
