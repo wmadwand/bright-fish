07ea0b967b3c0fdb9e789dfb3866168d42c7b83d
wmadwand <wmadwand@gmail.com>
2019-07-02 10:16:39 +0300

    Replace score Text with TextMeshPro component

--- Assets/_Project/Scripts/CoinScoreText.cs
+++ Assets/_Project/Scripts/CoinScoreText.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CoinScoreText : MonoBehaviour
 {
-	public Text text;
+	public TextMeshProUGUI text;
 	public float showTime = 1.5f;
 
 	public void SetScore(int value, bool wrongCoin = false)
 	{
 		text.color = wrongCoin ? Color.red : Color.white;
 
-		var str = wrongCoin ? "-" : "";
+		var str = wrongCoin ? "-" : "+";
 		text.text = $"{str}{value}";
 	}
 
