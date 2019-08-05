4c5915a19a63421a1e7af30152bb98fb76f2be42
wmadwand <wmadwand@gmail.com>
2019-08-05 20:08:55 +0300

    Import images. Script draft version

---
+++ Assets/_Project/Scripts/PauseButton.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BrightFish
+{
+	public class PauseButton : MonoBehaviour
+	{
+		public Toggle m_MenuToggle;
+		private float m_TimeScaleRef = 1f;
+		private float m_VolumeRef = 1f;
+		private bool m_Paused;
+
+
+		void Awake()
+		{
+			//m_MenuToggle = GetComponent<Toggle>();
+		}
+
+
+		private void MenuOn()
+		{
+			m_TimeScaleRef = Time.timeScale;
+			Time.timeScale = 0f;
+
+			m_VolumeRef = AudioListener.volume;
+			AudioListener.volume = 0f;
+
+			m_Paused = true;
+
+			Debug.Log("Pause");
+		}
+
+
+		public void MenuOff()
+		{
+			Time.timeScale = m_TimeScaleRef;
+			AudioListener.volume = m_VolumeRef;
+			m_Paused = false;
+
+			Debug.Log("Play");
+		}
+
+
+		public void OnMenuStatusChange()
+		{
+			if (!m_MenuToggle.isOn && !m_Paused)
+			{
+				MenuOn();				
+			}
+			else if (m_MenuToggle.isOn && m_Paused)
+			{
+				MenuOff();
+			}
+		}
+
+
+#if !MOBILE_INPUT
+	void Update()
+	{
+		if(Input.GetKeyUp(KeyCode.Escape))
+		{
+		    m_MenuToggle.isOn = !m_MenuToggle.isOn;
+            Cursor.visible = m_MenuToggle.isOn;//force the cursor visible if anythign had hidden it
+		}
+	}
+#endif
+
+	}
+}
