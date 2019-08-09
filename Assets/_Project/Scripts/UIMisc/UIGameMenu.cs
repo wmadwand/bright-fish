8fa4c4af2050c0d1e81ea055766ec3c16a605bc1
wmadwand <wmadwand@gmail.com>
2019-08-09 12:24:39 +0300

    Arrange UI screens

---
+++ Assets/_Project/Scripts/UIMisc/UIGameMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameMenu : MonoBehaviour
+{
+	public GameObject mainScreen;
+	public GameObject chooseLevelScreen;
+	public GameObject howToPlayScreen;
+
+	public void ChooseLevelScreen()
+	{
+		mainScreen.SetActive(false);
+		chooseLevelScreen.SetActive(true);
+		howToPlayScreen.SetActive(false);
+	}
+
+	public void HowToPlayScreen()
+	{
+		mainScreen.SetActive(false);
+		chooseLevelScreen.SetActive(false);
+		howToPlayScreen.SetActive(true);
+	}
+
+	public void MainScreen()
+	{
+		mainScreen.SetActive(true);
+		chooseLevelScreen.SetActive(false);
+		howToPlayScreen.SetActive(false);
+	}
+}
